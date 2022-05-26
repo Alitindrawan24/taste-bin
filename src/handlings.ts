ae384ecf709248940828d866034ba2d4a80f3bf8
Alitindrawan24 <alitindrawan71@gmail.com>
2022-05-27 00:02:21 +0800

    Add slack to env

--- src/handlings.ts
+++ src/handlings.ts
@@ -5,7 +5,9 @@ const errorToSlack = require('express-error-slack')
 class Handlings {
     public handlings(app: Application): void {       
         dotenv();
-        app.use(errorToSlack({ webhookUri: process.env.SLACK_WEBHOOK_URI }))
+        if(process.env.NODE_ENV === 'production'){
+            app.use(errorToSlack({ webhookUri: process.env.SLACK_WEBHOOK_URI }))
+        }
     }
 }
 
