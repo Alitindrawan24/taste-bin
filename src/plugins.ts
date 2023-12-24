4f09b8d62c04158a78808551dea630d6b114ee00
Alit Indrawan <alitindrawan71@gmail.com>
2023-12-25 02:05:07 +0800

    sdd rate limiter

--- src/plugins.ts
+++ src/plugins.ts
@@ -1,5 +1,6 @@
 import express, { Application } from 'express';
 import PrettyError from 'pretty-error';
+import { rateLimit } from 'express-rate-limit'
 class Plugins {
     public plugins(app: Application): void {       
         new PrettyError().withoutColors().start();
@@ -8,6 +9,14 @@ class Plugins {
 
         app.use(express.static('public'))
         app.use(express.urlencoded({extended: true}))
+
+        const limiter = rateLimit({
+            windowMs: 1 * 60 * 1000, // 1 minutes
+            max: 10, // Limit each IP to 10 requests per `window` (here, per 1 minutes)
+            standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+            legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+        })
+        app.use(limiter)
     }
 }
 
